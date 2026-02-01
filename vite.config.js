864ac11e2af0c45fba4521786e85139b546ee75f
Enriko Attila Wahyu <enrikoattilaw14@gmail.com>
2026-02-01 15:39:04 +0900

    Update vite.config.js base path for custom repository

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // [https://vitejs.dev/config/](https://vitejs.dev/config/)
 export default defineConfig({
   plugins: [react()],
-  base: "/https://enrikoaw.github.io/Template1//", // Ganti dengan nama repository kamu di GitHub
+  base: "/custom/", // Ganti dengan nama repository kamu di GitHub
 })
